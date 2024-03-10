64a794db044e43ccec66d3819b8d363bb040b15e
Michal Procházka <michal@prochazkaml.eu>
2024-03-10 21:28:38 +0100

    Microphone module can now have its string customized

--- src/module/microphone.rs
+++ src/module/microphone.rs
@@ -1,16 +1,24 @@
 use crate::config;
 use crate::modules;
+use crate::getdata;
+use crate::configoptional;
 
 use pulsectl::controllers::SourceController;
 use pulsectl::controllers::AppControl;
 
-pub fn init(_config: &Vec<config::ConfigKeyValue>) -> Result<Vec<modules::ModuleData>, String> {
-	let data: Vec<modules::ModuleData> = Vec::new();
+enum Data {
+    ACTIVESTRING
+}
+
+pub fn init(config: &Vec<config::ConfigKeyValue>) -> Result<Vec<modules::ModuleData>, String> {
+	let mut data: Vec<modules::ModuleData> = Vec::new();
+
+    configoptional!("_active", TypeString, "active", data, config);
 
 	Ok(data)
 }
 
-pub fn run(_data: &Vec<modules::ModuleData>, _ts: std::time::Duration) -> Result<Option<String>, String> {
+pub fn run(data: &Vec<modules::ModuleData>, _ts: std::time::Duration) -> Result<Option<String>, String> {
     let mut handler = match SourceController::create() {
         Ok(val) => val,
         Err(_) => { return Err("PulseAudio conn error".to_string()); }
@@ -21,10 +29,12 @@ pub fn run(_data: &Vec<modules::ModuleData>, _ts: std::time::Duration) -> Result
         Err(_) => { return Ok(None); }
     };
 
+    getdata!(activestr, ACTIVESTRING, TypeString, data);
+
     if apps.len() <= 0 {
         return Ok(None);
     } else {
-        return Ok(Some("active".to_string()));
+        return Ok(Some(activestr.to_string()));
     }
 }
 
